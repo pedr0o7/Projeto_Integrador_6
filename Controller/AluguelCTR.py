3ae6c558f7223f732ed8a369b0aea4e19e0a9dc9
Pedro Rafael Faria Ferreira <58218137+pedr0o7@users.noreply.github.com>
2021-08-17 16:36:10 -0300

    Create AluguelCTR.py

---
+++ Controller/AluguelCTR.py
@@ -0,0 +1,43 @@
+from Model.DTO.AluguelDTO import AluguelDTO
+from Model.DAO.AluguelDAO import AluguelDAO
+
+class AluguelCTR:
+    def CadastrarAluguel(self,DataAluguel, DataPrazo, DataDevolucao, ValorAluguel,
+                      ValorMulta, KmEntrada, KmSaida, CodigoCli, CodigoVeic):
+        aluguelDTO = AluguelDTO
+        aluguelDTO.DataAluguel = DataAluguel
+        aluguelDTO.DataPrazo = DataPrazo
+        aluguelDTO.DataDevolucao = DataDevolucao
+        aluguelDTO.ValorAluguel = ValorAluguel
+        aluguelDTO.ValorMulta = ValorMulta
+        aluguelDTO.KmEntrada = KmEntrada
+        aluguelDTO.KmSaida = KmSaida
+        aluguelDTO.CodigoCli = CodigoCli
+        aluguelDTO.CodigoVeic = CodigoVeic
+
+        aluguelDAO = AluguelDAO
+        aluguelDAO.CadastrarAluguel(self,aluguelDTO)
+
+    def PesquisarTodosAluguel():
+
+        aluguelDAO = AluguelDAO
+        query = aluguelDAO.PesquisarTodosAluguel()
+
+        return query
+
+    def PesquisarAluguel(self,valor, tipo):
+        aluguelDAO = AluguelDAO
+        query = aluguelDAO.PesquisarAluguel(valor, tipo)
+
+        return query
+
+    def DevolverVeiculo(self,codigoAlug, dataDevol, valorMulta, kmSaida):
+        aluguelDTO = AluguelDTO
+
+        aluguelDTO.DataDevolucao = dataDevol
+        aluguelDTO.ValorMulta = valorMulta
+        aluguelDTO.KmSaida = kmSaida
+
+
+        aluguelDAO = AluguelDAO
+        aluguelDAO.DevolverVeiculo(codigoAlug, aluguelDTO)
