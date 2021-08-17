b6cccc80eb953d72629bc8ce65869ed5c6e8e365
Pedro Rafael Faria Ferreira <58218137+pedr0o7@users.noreply.github.com>
2021-08-17 16:34:10 -0300

    Create ClienteDTO.py

---
+++ Model/DTO/ClienteDTO.py
@@ -0,0 +1,8 @@
+class ClienteDTO:
+    def __init__(self):
+
+        self.Nome = None
+        self.CPF = None
+        self.Endereco = None
+        self.Email = None
+        self.Telefone = None
